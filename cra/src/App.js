f5256a97c0c3649f2f02dbd0c808041ad38f95aa
박지훈 <qkrw6@naver.com>
2021-09-23 13:50:16 +0900

    props 컴포넌트

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './components/Item';
 
+//  다수의 컴포넌트 렌더링
+const items = [
+  { id: 1, name: "Notebook", checked: true },
+  { id: 2, name: "Smartphone", checked: false },
+  { id: 3, name: "Tablet", checked: true },
+  { id: 4 },
+]
+
+const renderItem = item => <Item key={item.id} 
+  name={item.name} 
+  checked={item.checked} /> 
+function App() {
+  return (
+    <div className="App">
+      { items.map(renderItem)}
+      {/* <Item name="Notebook" checked={true}></Item>
+      <Item name="Smartphone" checked={false}></Item>
+      <Item name="Tablet" checked></Item>
+      <Item name="Monitor"></Item> */}
+    </div>
+  )
+}
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         첫 번째 리엑트 앱입니다.
+          첫 번째 리액트 앱입니다.
         </p>
         <a
           className="App-link"
@@ -21,5 +45,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
