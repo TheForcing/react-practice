fab4de20417790c13ce6f488de5490da4c5d1e87
박지훈 <qkrw6@naver.com>
2021-09-24 16:13:31 +0900

    Router

---
+++ cra/src/components/ContextChildren2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Consumer } from './ContextApi'; //데이터 소비자 임포트
+
+class ContextChildren2 extends React.Component {
+    // //컨택스트 이용하기 예제
+    // render(){
+    //     return(
+    //       <Consumer>
+    //           { contextValue=> 
+    //           <h3>contextValue ${contextValue}</h3>}
+    //       </Consumer>
+    //     )
+    // }
+   // 컨택스트로 부모 데이터 변경 예제
+   render() {
+       return (
+           <Consumer>
+               { value =>
+               <button onClick={e => value.setStateFunc("Message from Child")}>
+                   { value.name } Button
+               </button> 
+               }
+           </Consumer>
+       )
+   }
+}
+
+export default ContextChildren2;
--- cra/src/index.js
+++ cra/src/index.js
@@ -5,16 +5,26 @@ import './index.css';
 // import App from './App.props';
 // import App from './App.state';
 // import App from './App.function'; //함수형 컴포넌트 테스트
-import App from './App.ref';
+// import App from './App.ref';
+// import App from './App.context';
 import reportWebVitals from './reportWebVitals';
+// 일반적인 렌더링
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// React Routing
+import { BrowserRouter } from'react-router-dom';
+import App from './App.routing';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>
+  , document.getElementById("root")
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
